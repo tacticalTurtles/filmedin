b6457473a86c44acb146caa4f9982fa4f3129585
ryanmgray <gray.ryanm@gmail.com>
2017-02-08 15:41:11 -0800

    Fix import on now playing component

--- client/components/Nowplaying.jsx
+++ client/components/Nowplaying.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import NowplayingEntry from './NowplayingEntry.jsx';
 import testData from '../data/onConnect.js';
 import helpers from '../lib/helpers'
 
