335cda0872b7dee352a444625daa3983aef9d8e6
xfredlix <fred.m.li.93@gmail.com>
2017-02-09 15:17:12 -0800

    added styling and img format on the movies

--- client/components/Nowplaying.jsx
+++ client/components/Nowplaying.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
 import helpers from '../lib/helpers'
+import movies from '../data/onConnect';
 
 class Nowplaying extends React.Component {
   constructor() {
     super();
     this.state = {
-      currentMovies: [],
+      currentMovies: movies,
       apikey: 'vah23z92k8mvpscqy7nrg44e'
     };
-    helpers.getMovies().then(movies=> {
-      console.log(movies);
-      this.setState({
-        currentMovies: movies.data
-      });
-    });
+    // helpers.getMovies().then( movies => {
+      // console.log(movies);
+      // this.setState({
+      //   currentMovies: movies
+      // });
+    // });
   }
 
   render() {
@@ -22,25 +23,24 @@ class Nowplaying extends React.Component {
     <Carousel className="carouselClass" indicators={false}>
       {this.state.currentMovies.map(movie =>
         <Carousel.Item>
-          <div className="col-md-12 carousel-title-box">
-            <h4 className="carousel-title">[{movie.title.split(' ').join('_')}]</h4>
+          <div className="col-md-3 carousel-movie-img">
+            <img className="movie-img" src={movie.preferredImage.uri} />
           </div>
-          <div className="col-md-12">
+          <div className="col-md-9 carousel-movie-text">
+            <h4 className="carousel-title carousel-title-box">[{movie.title.split(' ').join('_')}]</h4>
             <p className="carousel-description">{movie.shortDescription}</p>
             <h8 className='carousel-showtimes-title'>Genres: { movie.genres ? movie.genres.join(', ') : 'N/A'}</h8>
-
-          </div>
-          <div className="carousel-showtimes">
-            <div>
-              <h8 className="carousel-showtimes-title col-md-12">Showtimes</h8>
+            <div className="carousel-showtimes">
+              <h8 className="carousel-showtimes-title">Upcoming Showtimes:</h8>
+              <p>
+                {movie.showtimes.map(time =>
+                  <a href={time.ticketURI}>{
+                      time.dateTime.slice(5).split('T').join(' ')}  | </a>
+                  )}
+                </p>
+              </div>
             </div>
-            {movie.showtimes.map(time =>
-              <h8><a href={time.ticketURI}>{
-                  time.dateTime.slice(5).split('T').join(' ')}  | </a></h8>
-            )}
-          </div>
         </Carousel.Item>
-
       )}
     </Carousel>
   );
