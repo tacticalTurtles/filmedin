c4361f6996ec054065df48ce5c229631a02ed61e
ryanmgray <gray.ryanm@gmail.com>
2017-02-09 13:46:14 -0800

    Add route for handling setting preferred genre

--- server/server.js
+++ server/server.js
@@ -38,6 +38,7 @@ app.get('/topics', forumController.getTopics);
 app.get('/messagesByTopicID', messageController.getMessagesByTopicID);
 app.post('/postTopic', forumController.postNewTopic);
 app.post('/postMessage', messageController.postNewMessage);
+app.post('/setFavoriteGenre', routeHelpers.updatePreferredGenre);
 
 
 module.exports = app;
