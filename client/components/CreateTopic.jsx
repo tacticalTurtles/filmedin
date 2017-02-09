65fce13cf4c67774f7d6bbfabb097117e451e711
andrewregal <andrew.regal@gmail.com>
2017-02-08 19:27:09 -0800

    Implement postMessage helper and connect to forum create message form.

--- client/components/CreateTopic.jsx
+++ client/components/CreateTopic.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import TextFieldGroup from './common/TextFieldGroup';
 
 class CreateTopic extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div>
-        <TextFieldGroup />
+        <TextFieldGroup
+          userID={this.props.userID}
+        />
       </div>
     )
   }
+}
+
+CreateTopic.propTypes = {
+  userID: React.PropTypes.number.isRequired
 }
 
 export default CreateTopic;
