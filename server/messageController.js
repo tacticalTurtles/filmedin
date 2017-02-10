af978a200fc7b4b1a38257c7d3f8114f1fb13756
xfredlix <fred.m.li.93@gmail.com>
2017-02-10 15:10:08 -0800

    commented out object deconstruction line

--- server/messageController.js
+++ server/messageController.js
@@ -2,7 +2,7 @@ var db = require('./db/db');
 
 module.exports = {
   postNewMessage: (req, res, next) => {
-    const { topicID, topicMessage, userID } = req.body;
+    // const { topicID, topicMessage, userID } = req.body;
     const queryStr = `insert into message (userID, topicID, message) values ('${res.body.userID}','${res.body.topicID}','${res.body.topicMessage}')`;
     db.query(queryStr, (err, data) => {
       res.json(data);
