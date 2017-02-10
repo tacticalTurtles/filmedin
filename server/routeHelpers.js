c239211d76767adb55b074ff61e54b89dd7731f6
xfredlix <fred.m.li.93@gmail.com>
2017-02-10 15:08:33 -0800

    changed object deconstruction

--- server/routeHelpers.js
+++ server/routeHelpers.js
@@ -292,7 +292,7 @@ module.exports = {
 
   updatePreferredGenre: (req, res, next) => {
     const {category, id} = req.body;
-    db.profile.updatePreferredGenre(category, id, (err, rows) => {
+    db.profile.updatePreferredGenre(req.body.category, req.body.id, (err, rows) => {
       console.error(err);
       res.send();
     })
