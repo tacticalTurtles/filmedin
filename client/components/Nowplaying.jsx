0d855a6ba6bd7c463e172c94a1399b81e4ee81b9
xfredlix <fred.m.li.93@gmail.com>
2017-02-09 19:00:43 -0800

    changed it to be a state so it'll change the drop down everytime it changes it

--- client/components/Nowplaying.jsx
+++ client/components/Nowplaying.jsx
@@ -4,8 +4,8 @@ import helpers from '../lib/helpers'
 import movies from '../data/onConnect';
 
 class Nowplaying extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       currentMovies: movies,
       apikey: 'vah23z92k8mvpscqy7nrg44e'
@@ -18,18 +18,54 @@ class Nowplaying extends React.Component {
     // });
   }
 
+  removeLeastPreferred() {
+    const {user} = this.props;
+    let movieCopy = this.state.currentMovies.slice(0);
+    for (let i = 0; i < movieCopy.length; i++) {
+      for (let genre of movieCopy[i].genres) {
+        if (genre.toLowerCase() === user.leastPreferredGenre) {
+          movieCopy.splice(i, 1);
+          i --;
+        }
+      }
+    }
+    this.setState({
+      currentMovies: movieCopy
+    }, this.sortMovies);
+  }
+
+  sortMovies() {
+    const {user} = this.props;
+    this.setState({
+      currentMovies: this.state.currentMovies.sort((a,b) => {
+        for ( let genre of b.genres) {
+          if (genre.toLowerCase() === user.preferredGenre) {
+            return 1
+          }
+        }
+        return -1
+      })
+    })
+  }
+
+  componentWillMount() {
+    this.removeLeastPreferred();
+  }
+
   render() {
     return (
     <Carousel className="carouselClass" indicators={false}>
       {this.state.currentMovies.map(movie =>
         <Carousel.Item>
+          <div className="col-md-12">
+            <h3 className="carousel-title">[{movie.title.split(' ').join('_')}]</h3>
+          </div>
           <div className="col-md-3 carousel-movie-img">
             <img className="movie-img" src={movie.preferredImage.uri} />
           </div>
           <div className="col-md-9 carousel-movie-text">
-            <h4 className="carousel-title carousel-title-box">[{movie.title.split(' ').join('_')}]</h4>
             <p className="carousel-description">{movie.shortDescription}</p>
-            <h8 className='carousel-showtimes-title'>Genres: { movie.genres ? movie.genres.join(', ') : 'N/A'}</h8>
+            <h8 className='carousel-genre'>Genres: { movie.genres ? movie.genres.join(', ') : 'N/A'}</h8>
             <div className="carousel-showtimes">
               <h8 className="carousel-showtimes-title">Upcoming Showtimes:</h8>
               <p>
